3e10d20bcb17cbaf039bb7ad02ffe5391700e6ef
Mujib <mujib517@gmail.com>
2022-06-15 08:48:16 +0530

    Update and Delete

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -11,6 +11,13 @@ const get = async (req, res) => {
     }
 };
 
+const getById = async (req, res) => {
+    const id = req.params.id;
+    const product = await ProductModel.findOne({ _id: id }, { __v: 0 });
+    res.status(200);
+    res.json(product);
+};
+
 const post = async (req, res) => {
     try {
         const product = new ProductModel(req.body);
@@ -23,4 +30,36 @@ const post = async (req, res) => {
     }
 };
 
-module.exports = { get, post };
+const remove = async (req, res) => {
+    const id = req.params.id;
+    await ProductModel.deleteOne({ _id: id });
+
+    res.status(204);
+    res.send();
+};
+
+// full update
+// CRUD: Create, Read, Update, Delete
+const update = async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    await ProductModel.findOneAndUpdate({ _id: id }, {
+        brand: body.brand,
+        model: body.model,
+        price: body.price,
+        inStock: body.inStock,
+        category: body.category,
+    });
+
+    res.status(204);
+    res.send();
+};
+
+module.exports = {
+    get,
+    post,
+    getById,
+    remove,
+    update
+};
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,5 +5,10 @@ const router = express.Router();
 // http://localhost:3000/api/products/
 router.get('/', productCtrl.get);
 router.post('/', productCtrl.post);
+// http://localhost:3000/api/products/akdfjdk123434
+router.get('/:id', productCtrl.getById);
+router.delete('/:id', productCtrl.remove);
+router.put('/:id', productCtrl.update);
+// DELETE http://localhost:3000/api/products/:id
 
 module.exports = router;
