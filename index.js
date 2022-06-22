a0fd7adef10f86d88612210c694403d837d1a78b
Mujib <mujib517@gmail.com>
2022-06-22 09:05:25 +0530

    middlwares and basic auth

--- index.js
+++ index.js
@@ -44,7 +44,40 @@ mongoose.connect(config.dbConStr, (err, result) => {
     }
 });
 
+function authenticate(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        // verify credentials
+        const tokens = authHeader.split(' ');
+        const encodedStr = tokens[1];
+        let buff = new Buffer(encodedStr, 'base64');
+        let decodedStr = buff.toString('utf-8');
+
+        const credentials = decodedStr.split(':');
+        const username = credentials[0];
+        const password = credentials[1];
+
+        if (username === 'admin' && password === 'password') {
+            next();
+        }
+        else {
+            res.status(401);
+            res.send('Unauthorized');
+        }
+
+    } else {
+        res.status(401);
+        res.send('Unauthorized');
+    }
+}
+
+// public
+// encoding 
+app.use(authenticate);
 app.use('/', homeRouter);
+
+
+// private
 app.use('/api/products', productRouter);
 app.use('/api/reviews', reviewRouter);
 
